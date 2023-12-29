eaa597865e13464b2b128fad7a26966941b2c373
ys-00 <yassir7m7@gmail.com>
2023-12-29 17:54:23 +0100

    mantain my battery-notify percentage

--- .config/scripts/battery-notify.sh
+++ .config/scripts/battery-notify.sh
@@ -4,6 +4,9 @@ PREVIOUS=$(acpi -a | grep -o 'on-line')
 
 while true; do
  CURRENT=$(acpi -a | grep -o 'on-line')
+ battery_level_0=$(acpi -b | awk -F'[,:]' '/Battery 0/{print $3}' | tr -dc '0-9')
+ battery_level_1=$(acpi -b | awk -F'[,:]' '/Battery 1/{print $3}' | tr -dc '0-9')
+
  if [ "$CURRENT" != "$PREVIOUS" ]; then
     if [ "$CURRENT" == "on-line" ]; then
       notify-send -i ~/.config/dunst/icons/battery-plugged-in.png "Charger Plugged In !"
@@ -12,17 +15,14 @@ while true; do
     fi
     PREVIOUS=$CURRENT
  fi
- sleep 1
-done
 
-while true
-do
- battery_level_0=$(acpi -b | awk -F'[,:]' '/Battery 0/{print $3}' | tr -dc '0-9')
-
- if [ "${battery_level_0}" -lt 30 ]; then
- notify-send -i ~/.config/dunst/icons/low-battery.png "Battery 0 is below 30%!" "Charging: ${battery_level_0}%"
+ if (( battery_level_0 < 30 )); then
+   notify-send -i ~/.config/dunst/icons/low-battery.png "Battery 0 is below 99%!" "Charging: ${battery_level_0}%"
  fi
 
- sleep 90 # 300 seconds or 5 minutes
-done
+ if (( battery_level_1 < 30 )); then
+   notify-send -i ~/.config/dunst/icons/low-battery.png "Battery 1 is below 20%!" "Charging: ${battery_level_1}%"
+ fi
 
+ sleep 20 # 20 seconds
+done
