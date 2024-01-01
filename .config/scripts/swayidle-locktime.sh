435a64c345841fda06e15eeb7cf3b76d5d45b36b
ys-00 <yassir7m7@gmail.com>
2024-01-01 13:44:13 +0100

    some tweaks

--- .config/scripts/swayidle-locktime.sh
+++ .config/scripts/swayidle-locktime.sh
@@ -4,13 +4,18 @@ pgrep_output=$(pgrep swayidle)
 pgrep_arr=($pgrep_output)
 
 if [[ "${#pgrep_arr[@]}" == "0" ]]; then
+
  echo "Swayidle is not running. Starting Swayidle."
+
  swayidle -w \
     timeout 20 'swaylock -f' \
     timeout 20 'hyprctl dispatch dpms off' \
     resume 'hyprctl dispatch dpms on' &
+
 else
+
  echo "Swayidle is running. Killing swayidle."
  killall swayidle
+
 fi
 
