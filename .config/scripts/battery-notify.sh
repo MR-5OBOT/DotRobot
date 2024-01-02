592f0d0ffe70c4f254373203507b07fa9953a663
ys-00 <yassir7m7@gmail.com>
2024-01-02 21:44:34 +0100

    battery notify is perfect i think

--- .config/scripts/battery-notify.sh
+++ .config/scripts/battery-notify.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
-PREVIOUS=$(acpi -a | grep -o 'on-line')
-battery_level_0=$(acpi -b | awk -F'[,:]' '/Battery 0/{print $3}' | tr -dc '0-9')
+# Function to check charger status
+check_charger() {
+   while true; do
+       PREVIOUS=$(acpi -a | grep -o 'on-line')
+       sleep 1
+       CURRENT=$(acpi -a | grep -o 'on-line')
+       if [ "$CURRENT" != "$PREVIOUS" ]; then
+           if [ "$CURRENT" == "on-line" ]; then
+               notify-send -i ~/.config/dunst/icons/battery-plugged-in.png "Charger Plugged In !"
+               paplay ~/.config/dunst/sounds/device-added.oga
+           else
+               notify-send -i ~/.config/dunst/icons/battery-unplugged.png "Charger Unplugged !"
+               paplay ~/.config/dunst/sounds/device-removed.oga
+           fi
+       fi
+   done
+}
 
-while true; do
- CURRENT=$(acpi -a | grep -o 'on-line')
- if [ "$CURRENT" != "$PREVIOUS" ]; then
- if [ "$CURRENT" == "on-line" ]; then
-   paplay ~/.config/dunst/sounds/device-added.oga
-   notify-send -i ~/.config/dunst/icons/battery-plugged-in.png "Charger Plugged In !"
- else
-   paplay ~/.config/dunst/sounds/device-removed.oga
-   notify-send -i ~/.config/dunst/icons/battery-unplugged.png "Charger Unplugged !"
- fi
-  PREVIOUS=$CURRENT
- fi
+# Function to check battery level
+check_battery() {
+   while true; do
 
-sleep 6 
+       battery_level_0=$(acpi -b | awk -F'[,:]' '/Battery 0/{print $3}' | tr -dc '0-9')
+       battery_level_1=$(acpi -b | awk -F'[,:]' '/Battery 1/{print $3}' | tr -dc '0-9')
 
- if [[ "${battery_level_0}" -lt 90 ]]; then
-   paplay ~/.config/dunst/sounds/battery-low.wav
-   notify-send -i ~/.config/dunst/icons/low-battery.png "Battery 0 is below 20%!" "Charging: ${battery_level_0}%"
- fi
+       if [ "${battery_level_0}" -lt 20 ]; then
+           notify-send -i ~/.config/dunst/icons/low-battery.png "Battery 0 is below 20%!" "Charging: ${battery_level_0}%"
+           paplay ~/.config/dunst/sounds/battery-low.wav
+       fi
 
-echo "PID of this script: $$"
+       if [ "${battery_level_1}" -lt 10 ]; then
+           notify-send -i ~/.config/dunst/icons/low-battery.png "Battery 1 is below 10%!" "Charging: ${battery_level_1}%"
+           paplay ~/.config/dunst/sounds/battery-low.wav
+       fi
+       sleep 60
+   done
+}
 
-done
+# Start the functions in the background
+check_charger &
+check_battery &
+
+# Wait for all background processes to finish
+wait
