df4bc909fb7822956b738e477021d90e4cb6a515
ys-00 <yassir7m7@gmail.com>
2024-01-07 22:27:25 +0100

    stable dotfiles v1 cool

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/bash
 
 # exec-once = ~/.config/scripts/mako-start
-exec-once = swww init
-exec-once = ~/.config/scripts/dunst-start
-exec-once = ~/.config/scripts/togglebar.sh
-exec-once = ~/.config/scripts/rame-treshold.sh
-exec-once = ~/.config/scripts/battery-notify.sh
-exec-once = ~/.config/scripts/swayidle-locktime.sh
-exec-once = ~/.config/scripts/swww-randomize.sh
+exec-once = ~/.config/scripts/dunst-start # notification app
+exec-once = ~/.config/scripts/togglebar.sh # toggle waybar
+exec-once = ~/.config/scripts/battery-notify.sh # get battery notifications
+exec-once = ~/.config/scripts/swayidle-locktime.sh # swayidle lockscreen with swaylock
+exec-once = ~/.config/scripts/swww-randomize.sh # random wallpapers
 
-exec-once = nm-applet &
+exec-once = swww init
+exec-once = nm-applet & # network manager tray
+exec-once = udiskie & # Automounter for removable media
+exec-once = devify & # Notify about devices connecting and disconnecting
+exec-once = swayidle & # Idle daemon to screen lockh
+
 exec-once = wl-clipboard-history -t
-exec-once = wl-paste --type text --watch cliphist store #Stores only text data
-exec-once = wl-paste --type image --watch cliphist store #Stores only image data
+exec-once = wl-paste --type text --watch cliphist store &  #Stores only text data
+exec-once = wl-paste --type image --watch cliphist store & #Stores only image data
 
 exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED BY HYPRLAND DOC)
-exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 exec-once = ~/.config/hypr/xdg-portal-hyprland
-exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+exec-once = dbus-update-activation-environment --all &
+exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 
