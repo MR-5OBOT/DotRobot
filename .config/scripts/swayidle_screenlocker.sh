0c1454f711d2184dfb88e07355fe133ee87429c1
ys-00 <yassir7m7@gmail.com>
2024-01-28 01:48:31 +0100

    .

--- .config/scripts/swayidle_screenlocker.sh
+++ .config/scripts/swayidle_screenlocker.sh
@@ -4,6 +4,6 @@ paplay /usr/share/sounds/freedesktop/stereo/service-login.oga
 notify-send -i ~/.config/dunst/icons/sleep-timer.png "Swayidle is not running. Starting Swayidle!"
 
 swayidle -w \
-   timeout 60 'swaylock -f' \
-   timeout 60 'hyprctl dispatch dpms off' \
+   timeout 90 'swaylock -f' \
+   timeout 90 'hyprctl dispatch dpms off' \
    resume 'hyprctl dispatch dpms on' &
