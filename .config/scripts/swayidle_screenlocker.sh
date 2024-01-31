e18b1d93ab217c2cf2ac0a237081cc44b6b7f267
ys-00 <yassir7m7@gmail.com>
2024-01-31 17:21:13 +0100

    make the notification msg more short

--- .config/scripts/swayidle_screenlocker.sh
+++ .config/scripts/swayidle_screenlocker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 paplay /usr/share/sounds/freedesktop/stereo/service-login.oga
-notify-send -i ~/.config/dunst/icons/sleep-timer.png "Swayidle is not running. Starting Swayidle!"
+notify-send -i ~/.config/dunst/icons/sleep-timer.png "Starting Swayidle!"
 
 swayidle -w \
    timeout 120 'gtklock -i -s ~/.config/gtklock/style.css ' \
