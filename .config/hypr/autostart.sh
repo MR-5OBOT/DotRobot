88cc9973da445ce6767034a76a3b94ad05dabf44
MR-5OBOT <yassir7m7@gmail.com>
2024-02-04 23:08:26 +0100

    5

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -5,20 +5,19 @@ scripts="$HOME/.config/hypr/scripts"
 wallpapers="$scripts""/wallpapers"
 autostart="$scripts""/autostart"
 
+dunst & # notfication demon
+swww kill ; swww init  # wallpaper demon
+nm-applet --indicator  # network manager tray
+blueman-applet # blueman-applet
+udiskie -t -n & # Automounter for removable media
+devify & # Notify about devices connecting and disconnecting
+hyprctl setcursor Volantes Light Cursors 25
+
 $scripts/lock.sh
-$autostart/rdunst.sh # notification app
 $autostart/toggle-waybar.sh # toggle waybar
 $autostart/idle_handler.sh # swayidle lockscreen with swaylock
 $wallpapers/swww-randomize.sh # random wallpapers
 $wallpapers/low-battery-notify.sh
-hyprctl setcursor Volantes Light Cursors 25
-
-swww kill ; swww init  # wallpaper demon
-nm-applet --indicator &  # network manager tray
-blueman-applet  # blueman-applet
-udiskie -t -n & # Automounter for removable media
-devify & # Notify about devices connecting and disconnecting
-# swayidle & # Idle daemon to screen lockh
 
 wl-paste --type text --watch cliphist store &  #Stores only text data
 wl-paste --type image --watch cliphist store & #Stores only image data
@@ -30,5 +29,8 @@ $scripts/autostart/xdgportals
 dbus-update-activation-environment --all & 
 sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP 
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+
+
+
 
 
