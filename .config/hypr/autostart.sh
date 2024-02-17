474f3ba71beb01b1d87d81921f5cf9c8d1991b2c
MR_5OBOT <yassir7m7@gmail.com>
2024-02-17 17:07:15 +0100

    rofi & autostart.sh for hyprland

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -2,7 +2,6 @@
 
 $SCRIPTS=~/.config/hypr/scripts/
 
-
 exec-once = dunst & # notfication demon
 exec-once = nm-applet & # network manager tray
 exec-once = udiskie -t -n & # Automounter for removable media
@@ -13,13 +12,13 @@ exec-once = hyprctl setcursor Volantes Light Cursors 27
 exec-once = wl-paste --type text --watch cliphist store &  #Stores only text data
 exec-once = wl-paste --type image --watch cliphist store & #Stores only image data
 
-exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED BY HYPRLAND DOC)
-# exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-exec-once = $SCRIPTS/autostart/xdgportals
-
 exec-once = dbus-update-activation-environment --all & 
 exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP 
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+
+exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED BY HYPRLAND DOC)
+# exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+exec-once = $SCRIPTS/autostart/xdgportals
 
 exec-once = $SCRIPTS/autostart/lock.sh
 exec-once = $SCRIPTS/autostart/swww-daemon.sh
