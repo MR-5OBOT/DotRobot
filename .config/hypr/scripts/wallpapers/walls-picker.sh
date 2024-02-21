592a4d0dcf0a36af7d11e088a48fa60fee66a76f
MR_5OBOT <yassir7m7@gmail.com>
2024-02-21 16:26:06 +0100

    fix bugs 2

--- .config/hypr/scripts/wallpapers/walls-picker.sh
+++ .config/hypr/scripts/wallpapers/walls-picker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Set directory to wallpaper folder
 dir="$HOME/Pictures/wallpapers/"
@@ -7,7 +7,7 @@ dir="$HOME/Pictures/wallpapers/"
 images=( $(ls $dir) )
 
 # Use Rofi to display image selection menu
-selected=$(printf '%s\n' "${images[@]}" | rofi -dmenu -p "Select wallpaper image")
+selected=$(printf '%s\n' "${images[@]}" | rofi -dmenu -p "Select wallpaper image" -markup-rows)
 
 # If user selects an image, run command with selected image as argument
 if [[ -n $selected ]]; then
