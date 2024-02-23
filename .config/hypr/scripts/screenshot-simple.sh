9775b121af1ee4262f7fa64662fb6431e1461514
MR_5OBOT <yassir7m7@gmail.com>
2024-02-23 20:23:30 +0100

    fix screeenshot script

--- .config/hypr/scripts/screenshot-simple.sh
+++ .config/hypr/scripts/screenshot-simple.sh
@@ -3,5 +3,5 @@
 FILE_NAME="Screenshots-$(date +%F-%T).png"
 FILE_PATH="${HOME}/Pictures/Screenshots/${FILE_NAME}"
 
-grimblast --notify save area "$FILE_PATH"
+grimblast --notify copysave area "$FILE_PATH"
 # notify-send 'Screenshot' -i "${FILE_PATH}" "${FILE_NAME}"
