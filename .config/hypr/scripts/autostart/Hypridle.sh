a4654d877c00a183e87dfa4633447e3c280c3558
MR_5OBOT <yassir7m7@gmail.com>
2024-03-20 02:03:30 +0000

    after 2 weeks i think

--- .config/hypr/scripts/autostart/Hypridle.sh
+++ .config/hypr/scripts/autostart/Hypridle.sh
@@ -4,5 +4,5 @@ notify-send -i ~/.config/dunst/icons/sleep-timer.png "Starting Swayidle!"
 
 swayidle -w \
    timeout 120 'gtklock -i -s ~/.config/gtklock/style.css ' \
-   timeout 120 'hyprctl dispatch dpms off' \
+   timeout 12 'hyprctl dispatch dpms off' \
    resume 'hyprctl dispatch dpms on' &
