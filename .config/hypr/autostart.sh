5790443ed189c69c16c94ae1540a40ece6fb30df
MR_5OBOT <yassir7m7@gmail.com>
2024-06-14 17:48:05 +0100

    u

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -10,23 +10,23 @@ exec-once = devify &
 exec-once = swww init
 exec-once = udiskie --no-automount --smart-tray # front-end that allows to manage removable media
 exec-once = nm-applet --indicator # systray app for Network/Wifi
+exec-once = wl-paste --type text --watch cliphist store  # Stores only text data
+exec-once = wl-paste --type image --watch cliphist store # Stores only image data
 
 exec-once = $SCRIPTS/autostart/xdgportals
 exec-once = dbus-update-activation-environment --all & # for XDPH
 exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
-
-exec-once = hyprctl setcursor Volantes Light Cursors 27
 exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED BY HYPRLAND DOC)
-# exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-exec-once = wl-paste --type text --watch cliphist store  # Stores only text data
-exec-once = wl-paste --type image --watch cliphist store # Stores only image data
 
+exec-once = gsettings set org.gnome.desktop.interface cursor-theme volantes_light_cursors
+exec-once = hyprctl setcursor volantes_light_cursors 27
+
+exec-once = $SCRIPTS/autostart/gtk.sh
 exec-once = $SCRIPTS/wallpapers/random_walls2.sh
 exec-once = $SCRIPTS/autostart/Polkit.sh
 exec-once = $SCRIPTS/autostart/toggle-waybar.sh
 exec-once = $SCRIPTS/autostart/TinkPad_BT_notify.sh
-# exec-once = $SCRIPTS/autostart/lock.sh
 # exec-once = swaybg -i ~/Pictures/wallpapers/b.png
 
 
