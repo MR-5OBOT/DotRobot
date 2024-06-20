282e973fa0efd4aac13357f2598e8d351158d325
MR5OBOT <yassir7m7@gmail.com>
2024-06-20 15:52:20 +0100

    more tweaking

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -16,7 +16,7 @@ exec-once = nm-applet --indicator # systray app for Network/Wifi
 exec-once = wl-paste --type text --watch cliphist store  # Stores only text data
 exec-once = wl-paste --type image --watch cliphist store # Stores only image data
 
-exec-once = hyprctl setcursor volantes_light_cursors 27
+# exec-once = hyprctl setcursor volantes_light_cursors 27
 exec-once = dbus-update-activation-environment --all # for XDPH
 exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
