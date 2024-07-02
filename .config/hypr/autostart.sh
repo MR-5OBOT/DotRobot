1d4bc850b029305bc6b6391ca70057e00ca92307
MR5OBOT <yassir7m7@gmail.com>
2024-07-02 23:46:51 +0100

    ld

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -10,13 +10,15 @@ exec-once = devify
 exec-once = swww init
 # exec-once = hypridle
 
-exec-once = hyprctl setcursor Future-dark Cursors 24
 exec-once = udiskie --no-automount --smart-tray # front-end that allows to manage removable media
 exec-once = nm-applet --indicator # systray app for Network/Wifi
-# exec-once = wl-clipboard --watch -t
+
+exec-once = hyprctl setcursor Future-dark-cursors 24
+exec-once = wl-clipboard --watch -t
 exec-once = wl-paste --type text --watch cliphist store  # Stores only text data
 exec-once = wl-paste --type image --watch cliphist store # Stores only image data
 
+exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 || /usr/lib/polkit-gnome-authentication-agent-1
 exec-once = dbus-update-activation-environment --all
 exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # Some fix idk
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
@@ -24,8 +26,6 @@ exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED B
 
 exec-once = $SCRIPTS/autostart/xdgportals
 exec-once = $SCRIPTS/wallpapers/random_walls.sh
-# exec-once = $SCRIPTS/wallpapers/random_walls2.sh
-exec-once = $SCRIPTS/autostart/Polkit.sh
 exec-once = $SCRIPTS/autostart/toggle-waybar.sh
 exec-once = $SCRIPTS/autostart/TinkPad_BT_notify.sh
 # exec-once = $SCRIPTS/autostart/gtk.sh # Load GTK settings
