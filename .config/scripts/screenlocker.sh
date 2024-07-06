f8f95c8a3ee6828f3aa14444da973df813cbecd5
MR5OBOT <yassir7m7@gmail.com>
2024-07-06 10:05:54 +0100

    jd

--- .config/scripts/screenlocker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swayidle -w \
-     timeout 200 'swaylock -f' \
-     timeout 200 'swaymsg "output * dpms off"' \
-     resume 'swaymsg "output * dpms on"' \
-     before-sleep 'swaylock -f'
-
