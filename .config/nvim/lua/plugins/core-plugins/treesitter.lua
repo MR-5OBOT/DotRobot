103c8d9184d9e3d20acb21e2fe10c7270f2fa804
MR5OBOT <yassir7m7@gmail.com>
2024-07-18 21:36:24 +0100

    jd

--- .config/nvim/lua/plugins/core-plugins/treesitter.lua
+++ .config/nvim/lua/plugins/core-plugins/treesitter.lua
@@ -2,12 +2,24 @@ return {
   'nvim-treesitter/nvim-treesitter',
   config = function ()
   require'nvim-treesitter.configs'.setup {
-  	ensure_installed = { "lua", texlab },
+  	ensure_installed = {
+ 	  "lua",
+ 	  "python",
+    "markdown",
+    "markdown_inline",
+    "css",
+    "html",
+    },
   	sync_install = true,
-  	auto_install = true,
-  	highlight = {
-  		enable = true
-  	}
+    auto_install = true,
+    highlight = {
+    enable = true,
+    -- Some languages depend on vim's regex highlighting system (such as
+    -- Ruby) for indent rules. If you are experiencing weird indenting
+    -- issues, add the language to the list of additional vim regex
+    -- highlighting and disabled languages for indent.
+    additional_vim_regex_highlighting = { "ruby" },
+  },
   }
   end
 }
