017015267a4763c223194e438367b5d49224c50b
MR-5OBOT <yassir7m7@gmail.com>
2024-08-27 16:07:50 +0100

    try fixing xdgporrtals

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -7,7 +7,7 @@ exec-once = hyprctl setcursor Future-dark-cursors 24
 # Daemon
 exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 exec-once = dbus-update-activation-environment --systemd --all
-exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for xdg portals(screen sharing) 
+exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # make sure that xdg-portals get the required variables
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
 exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED BY HYPRLAND DOC)
 
@@ -25,6 +25,7 @@ exec-once = wl-paste --type text --watch cliphist store  # Stores only text data
 exec-once = wl-paste --type image --watch cliphist store # Stores only image data
 
 # scripts
+exec-once = ~/.local/bin/rame_check
 exec-once = $SCRIPTS/autostart/Hypridle.sh
 exec-once = $SCRIPTS/autostart/xdgportals.sh
 exec-once = $SCRIPTS/autostart/pipewire_check.sh
