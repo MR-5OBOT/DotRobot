69daeae2360b5fc477200b460ac04a9e266436e3
MR-5OBOT <yassir7m7@gmail.com>
2024-08-30 06:35:03 +0100

    add xwaylandvediobridge to autostart

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -2,13 +2,12 @@
 
 $SCRIPTS=~/.config/hypr/scripts
 
-# System
 exec-once = hyprctl setcursor Future-dark-cursors 24
-# Daemon
 exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 exec-once = dbus-update-activation-environment --systemd --all
+
+exec-once = xwaylandvideobridge
 exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # make sure that xdg-portals get the required variables
-exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # for XDPH
 exec-once = systemctl --user restart pipewire # RESTARTS PIPEWIRE (RECOMMENDED BY HYPRLAND DOC)
 
 exec-once = udiskie -a --tray # front-end that allows to manage removable media
@@ -16,8 +15,6 @@ exec-once = nm-applet --indicator # systray app for Network/Wifi
 exec-once = dunst
 exec-once = devify
 exec-once = hypridle
-# exec-once = sleep 3; blueman-applet
-# exec-once = swww init
 
 # clipboard deamon
 exec-once = wl-clip-persist --clipboard regular --write-timeout 1000
