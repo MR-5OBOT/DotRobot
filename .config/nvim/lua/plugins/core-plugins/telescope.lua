9b9c5f8775d075fa34c963cb4f672a9b2f24520b
MR-5OBOT <yassir7m7@gmail.com>
2024-08-31 02:21:54 +0100

    some nvim enhancement

--- .config/nvim/lua/plugins/core-plugins/telescope.lua
+++ .config/nvim/lua/plugins/core-plugins/telescope.lua
@@ -1,32 +1,40 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.4",
-	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-  local actions = require('telescope.actions')
-  require('telescope').setup {
-  defaults = {
-    layout_config = {
-      horizontal = {
-        prompt_position = "bottom",
-        preview_width = 0.55,
-        results_width = 0.8,
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  lazy = false,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local actions = require('telescope.actions')
+
+    require('telescope').setup {
+      defaults = {
+        file_ignore_patterns = { ".git/", "node_modules/", ".DS_Store" }, -- Patterns to ignore
+        layout_config = {
+          horizontal = {
+            prompt_position = "bottom",  -- Position of the prompt
+            preview_width = 0.55,        -- Width of the preview window
+            results_width = 0.8,         -- Width of the results window
+          },
+        },
+        prompt_prefix = "   ",          -- Custom prompt prefix
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next, -- Move down in insert mode
+            ["<C-k>"] = actions.move_selection_previous, -- Move up in insert mode
+            ["<esc>"] = actions.close, -- Close the picker
+          },
+          n = {
+            ["<C-j>"] = actions.move_selection_next, -- Move down in normal mode
+            ["<C-k>"] = actions.move_selection_previous, -- Move up in normal mode
+          }
+        }
       },
-    },
-   prompt_prefix = "   ",
-   mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<esc>"] = actions.close,
-      },
-      n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
+      pickers = {
+        find_files = {
+          hidden = true, -- Allow searching hidden files
+          find_command = {'rg', '--files', '--hidden', '--ignore', '--glob', '!.git/*'} -- Exclude .git files
+        },
       }
     }
-  },
-  }
   end,
 }
