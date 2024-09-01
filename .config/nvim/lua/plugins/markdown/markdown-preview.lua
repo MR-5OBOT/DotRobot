34a57a4c0edc10d582f320cc8800b013f2daff53
MR-5OBOT <yassir7m7@gmail.com>
2024-09-01 03:43:39 +0100

    a lot of work

--- .config/nvim/lua/plugins/markdown/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown/markdown-preview.lua
@@ -3,15 +3,16 @@ return {
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   build = "cd app && yarn install",
   init = function()
-  vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_filetypes = { "markdown" }
   end,
   ft = { "markdown" },
-  opts = {
-      vim.g.vim_markdown_folding_disabled == 1,
-      vim.g.vim_markdown_conceal == 0,
-      vim.g.vim_markdown_conceal_code_blocks == 0,
-      vim.g.vim_markdown_new_list_item_indent == 2,
-      vim.g.mkdp_theme == "dark",
-      vim.g.mkdp_filetypes == { "markdown" }
-    }
+  config = function()
+    -- Set options for markdown-preview.nvim
+    vim.g.vim_markdown_folding_disabled = 1
+    vim.g.vim_markdown_conceal = 0
+    vim.g.vim_markdown_conceal_code_blocks = 0
+    vim.g.vim_markdown_new_list_item_indent = 2
+    vim.g.mkdp_theme = "dark"
+    vim.g.mkdp_filetypes = { "markdown" }
+  end
 }
