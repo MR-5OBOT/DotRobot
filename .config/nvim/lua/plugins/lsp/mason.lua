8a67d3168bc89688e6cf88f27cf0248bba4a2be0
MR-5OBOT <yassir7m7@gmail.com>
2024-09-03 03:52:26 +0100

    remove some fonts

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -37,7 +37,7 @@ return {
         "prismals",
         "pyright",
         "texlab",
-        "ltex-ls"
+        "ltex"
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
