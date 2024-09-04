dacf52629023b069965275b8b90c03a90ad522ff
MR-5OBOT <yassir7m7@gmail.com>
2024-09-04 05:53:11 +0100

    try making the perfect Sys workflow

--- .config/nvim/lua/plugins/UX/toggle-term.lua
+++ .config/nvim/lua/plugins/UX/toggle-term.lua
@@ -16,7 +16,7 @@ return {
   autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
   close_on_exit = true, -- close the terminal window when the process exits
   start_in_insert = true,
-  direction = 'float',
+  direction = 'horizontal',
   auto_scroll = true, -- automatically scroll to the bottom on terminal output
 
   float_opts = {
--- .personal/remiders.py
+++ .personal/remiders.py
@@ -1,3 +1,7 @@
 # a simple python script to send reminders
 
+def yassir():
+    if yassir == 0:
+        print(0)
+
 
