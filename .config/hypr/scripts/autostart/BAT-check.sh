fb973cf21b7aca8584682e7026f31524b62a2e46
MR-5OBOT <yassir7m7@gmail.com>
2024-09-06 06:37:55 +0100

    .

---
+++ .config/hypr/scripts/autostart/BAT-check.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+if command -v acpi; then
+    echo "acpi is installed"
+else
+    echo "acpi is not installed"
+fi
+
+# infinite loop for the script to make it check the BAT0 every 5 minutes
+while true; do
+
+# get the battery levels
+get_percentage=$(acpi -b | grep -oP "\d+%" | head -n 1 | tr -d "%")
+bat0_percentage=$(acpi -b | head -n 1 )
+
+# check the BAT0 required Level
+if [[ "$get_percentage" -lt 60 ]]; then
+    # send notification
+    notify-send "$bat0_percentage"
+fi
+
+# check the battery for every 5 minutes 
+sleep 300
+
+done
