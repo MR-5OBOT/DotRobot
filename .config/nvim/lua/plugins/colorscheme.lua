9c7eda9f87a2890989b8b415b2e7bbb13460634e
MR-5OBOT <yassir7m7@gmail.com>
2024-09-27 16:31:08 +0100

    add new color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,18 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-		lazy = false,
-    priority = 999, -- make sure to load this before all the other start plugins
-		config = function()
-      vim.cmd.colorscheme("oxocarbon")
-	    vim.opt.background = "dark"
-			-- vim.cmd("highlight FloatBorder guifg=#202020")
-		end,
-}
+  -- Oxocarbon as the default color scheme
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = false,
+    priority = 999,                    -- Load before other plugins
+    config = function()
+      vim.cmd.colorscheme("oxocarbon") -- Set oxocarbon as default
+      vim.opt.background = "dark"      -- Ensure dark mode
+    end,
+  },
 
+  -- Monochrome color scheme
+  {
+    'kdheepak/monochrome.nvim',
+    lazy = true, -- Lazy load monochrome (optional, to save resources on startup)
+  },
+}
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -39,13 +39,35 @@ return {
 
     -- Setup LSP servers
     local capabilities = cmp_nvim_lsp.default_capabilities()
-    local servers = { html = {}, pyright = {}, lua_ls = {}, clangd = {}, gopls = {} }
+    local servers = { html = {}, pyright = {}, clangd = {}, gopls = {} }
 
+    -- Add Lua LSP with specific configuration
+    servers["lua_ls"] = {
+      settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT', -- Tell the language server to use LuaJIT (for Neovim)
+          },
+          diagnostics = {
+            globals = { 'vim' }, -- Recognize the `vim` global
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true), -- Make the server aware of Neovim runtime
+            checkThirdParty = false,                           -- Disable third-party library checking
+          },
+          telemetry = {
+            enable = false, -- Disable telemetry for privacy
+          },
+        },
+      },
+    }
+
+    -- Configure all servers
     for server, config in pairs(servers) do
-      lspconfig[server].setup({
+      lspconfig[server].setup(vim.tbl_extend("force", {
         capabilities = capabilities,
         on_attach = on_attach,
-      })
+      }, config))
     end
 
     -- Configure barbecue
