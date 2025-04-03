7b3e5fce6eddb559be791fb7d189918e051e71d3
MR-5OBOT <yassir7m7@gmail.com>
2025-04-03 08:52:03 +0000

    now works

--- .config/hypr/autostart.sh
+++ .config/hypr/autostart.sh
@@ -7,7 +7,7 @@ exec-once = hyprctl setcursor Future-dark-cursors 24
 exec-once = /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 exec-once = dbus-update-activation-environment --all
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # maket xdg-portals get required variables
+# exec-once = sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # maket xdg-portals get required variables
 
 # exec-once = udiskie -a --tray # front-end that allows to manage removable media
 exec-once = nm-applet --indicator
@@ -20,8 +20,8 @@ exec-once = wl-paste --type image --watch cliphist store
 
 exec-once = swaybg -i ~/Pictures/wallpapers/MR5OBOT.jpg
 exec-once = $SCRIPTS/autostart/toggle-waybar.sh
-# exec-once = $SCRIPTS/autostart/gtk.sh
+exec-once = $SCRIPTS/autostart/gtk.sh
 exec-once = $SCRIPTS/autostart/pipewire_check.sh
-exec-once = sleep 5 && $SCRIPTS/autostart/xdgportals.sh
+exec-once = sleep 2 && $SCRIPTS/autostart/xdgportals.sh
 exec-once = $SCRIPTS/autostart/Hypridle.sh
 exec-once = $SCRIPTS/autostart/BAT-check.sh
