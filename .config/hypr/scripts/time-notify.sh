44b6825d1c3ad0cce22ef7e88c34ffdf2fa4ddac
MR-5OBOT <yassir7m7@gmail.com>
2025-04-07 17:42:43 +0100

    .

--- .config/hypr/scripts/time-notify.sh
+++ .config/hypr/scripts/time-notify.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# Send notification
-notify-send -t 4500 "$(date '+%a %b %e %l%p')"
+# Get local time and New York time
+local_time=$(date '+%a %b %e %l:%M %p')
+new_york_time=$(TZ="America/New_York" date '+%a %b %e %l:%M %p')
 
+# Send notification with both times
+notify-send -t 4500 "Local Time: $local_time\nNew York Time: $new_york_time"
