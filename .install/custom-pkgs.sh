3e974fc20beb973bfeb3a087cc284a456a3d9489
MR-5OBOT <115130443+MR-5OBOT@users.noreply.github.com>
2025-04-22 01:16:10 +0100

    Add files via upload

--- .install/custom-pkgs.sh
+++ .install/custom-pkgs.sh
@@ -1,78 +1,77 @@
 #!/usr/bin/env bash
 
 packagesPacman=(
-	"viewnior"
-	"gimp"
-	"cliphist"
-	"obsidian"
-	"wf-recorder"
-	"tmux"
-	"prettier"
-	"swaybg"
-	"fd"
-	"rclone"
-	"figlet"
-	"imv"
-	"neovim"
-	"zsh"
-	"kitty"
-	"thunar"
-	"file-roller"
-	"fzf"
-	"eza"
-	"thunar-archive-plugin"
-	"thunar"
-	"zoxide"
-	"glow"
-	"trash-cli"
-	"mpv"
-	"tectonic"
-	"gvfs-google"
-	"gnome-online-accounts"
-	"kio-gdrive"
-	"rclone"
-	"dolphin"
-	# latex packages
-	# "tree-sitter-cli"
-	# "texlive-basic"
-	# "texlive-latex"
-	# "texlive-latexextra"
-	# "texlive-bibtexextra"
+    "viewnior"
+    "gimp"
+    "obsidian"
+    "wf-recorder"
+    "tmux"
+    "prettier"
+    "swaybg"
+    "fd"
+    "rclone"
+    "figlet"
+    "imv"
+    "neovim"
+    "zsh"
+    "kitty"
+    "thunar"
+    "file-roller"
+    "fzf"
+    "eza"
+    "thunar-archive-plugin"
+    "thunar"
+    "zoxide"
+    "glow"
+    "trash-cli"
+    "mpv"
+    "tectonic"
+    "gvfs-google"
+    "gnome-online-accounts"
+    "kio-gdrive"
+    "rclone"
+    "dolphin"
+    # latex packages
+    # "tree-sitter-cli"
+    # "texlive-basic"
+    # "texlive-latex"
+    # "texlive-latexextra"
+    # "texlive-bibtexextra"
 )
 
 packagesYay=(
-	"devify"
-	"wired"
-	# latex packages
-	# "pdflatex"
-	# "latexrun-git"
-	# "latex-mk"
-	# "texlive-binextra"
-	# "texlive-full"
+    "devify"
+    "wired"
+    # latex packages
+    # "pdflatex"
+    # "latexrun-git"
+    # "latex-mk"
+    # "texlive-binextra"
+    # "texlive-full"
 )
 
 # Function to install packages with Pacman, skipping already installed ones
 _installPackagesPacman() {
-	for package in "$@"; do
-		echo "Installing $package with Pacman..."
-		sudo pacman -S --needed --noconfirm $package
-		if [ $? -ne 0 ]; then
-			echo "Error: Failed to install $package with Pacman."
-			exit 1
-		fi
-	done
+    for package in "$@"; do
+        echo "Installing $package with Pacman..."
+        sudo pacman -S --needed --noconfirm $package
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to install $package with Pacman."
+            exit 1
+        fi
+    done
 }
 
 # Function to install packages with Yay, skipping already installed ones
 _installPackagesYay() {
-	for package in "$@"; do
-		echo "Installing $package with Yay..."
-		yay -S --needed --noconfirm $package
-		if [ $? -ne 0 ]; then
-			echo "Error: Failed to install $package with Yay."
-			exit 1
-		fi
-	done
+    for package in "$@"; do
+        echo "Installing $package with Yay..."
+        yay -S --needed --noconfirm $package
+        if [ $? -ne 0 ]; then
+            echo "Error: Failed to install $package with Yay."
+            exit 1
+        fi
+    done
 }
 
 # Main installation process
