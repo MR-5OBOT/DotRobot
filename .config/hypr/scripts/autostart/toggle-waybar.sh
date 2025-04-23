ede6aca253c73416f22fac29ab4f8f5f1df0d3cf
MR-5OBOT <yassir7m7@gmail.com>
2025-04-23 17:40:16 +0100

    xdgs not working

--- .config/hypr/scripts/autostart/toggle-waybar.sh
+++ .config/hypr/scripts/autostart/toggle-waybar.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Terminate already running bar instances
-
 killall -q waybar
-
-# Wait until the waybar processes have been shut down
 
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-# Launch main
-
-waybar -c ~/.config/waybar/config_vertical -s ~/.config/waybar/style_vertical.css
-
+# Log errors if waybar fails
+waybar -c ~/.config/waybar/config_vertical -s ~/.config/waybar/style_vertical.css >~/.cache/waybar.log 2>&1 &
