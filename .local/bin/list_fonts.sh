ce209f20bb822f68f69f7de86f1662eafd8acc57
MR-5OBOT <yassir7m7@gmail.com>
2025-04-23 01:53:19 +0100

    .

--- .local/bin/list_fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# MR5OBOT script for searching fonts with exact names
-
-read -p "Enter the font name prefix (e.g., IosevkaTerm): " prefix
-fc-list | grep -i "$prefix" | awk -F: '{print $2}' | tr ',' '\n' | sed 's/ *//g' | sort -u
