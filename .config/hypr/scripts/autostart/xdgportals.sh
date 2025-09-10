8b924e1a659cc3bad99a80f03d416c7ead5fdabf
MR-5OBOT <yassir7m7@gmail.com>
2025-09-10 21:23:27 +0100

    .

--- .config/hypr/scripts/autostart/xdgportals.sh
+++ .config/hypr/scripts/autostart/xdgportals.sh
@@ -8,22 +8,25 @@
 
 echo "starting xdgportals..."
 
-# Setup Timers
-_sleep1="0.1"
-_sleep2="0.5"
 
 # Kill all possible running xdg-desktop-portals
-killall -e xdg-desktop-portal-gnome
-killall -e xdg-desktop-portal-kde
-killall -e xdg-desktop-portal-lxqt
-killall -e xdg-desktop-portal-wlr
-killall -e xdg-desktop-portal-gtk
+# killall -e xdg-desktop-portal-hyprland
+# killall -e xdg-desktop-portal-gnome
+# killall -e xdg-desktop-portal-kde
+# killall -e xdg-desktop-portal-lxqt
+# killall -e xdg-desktop-portal-wlr
+# killall -e xdg-desktop-portal-gtk
+# killall xdg-desktop-portal
+# sleep 1
 
-sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall xdg-desktop-portal
+# set required environment variable
+# dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP # Set environment variables for XDG Desktop Portal
+
+# start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
 /usr/lib/xdg-desktop-portal &
+
+# start xdg-desktop-portal-gtk
+# /usr/lib/xdg-desktop-portal-gtk &
 
 echo "xdg portals script finish"
