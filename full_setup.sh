4b4708300c78c3f91ff374009862f7ca60107bf9
MR-5OBOT <yassir7m7@gmail.com>
2025-10-17 04:06:39 +0100

    ..

--- full_setup.sh
+++ full_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#  Function to run a script and check its exit status
+# Function to run a script and check its exit status
 run_script() {
 	local script=$1
 	echo "Running $script..."
@@ -15,23 +15,27 @@ run_script() {
 ## Full Setup for Hyprland Packages and Configurations
 echo "Starting full setup for MR5OBOT dotfiles"
 
-gum confirm "Do you want to start the full dotfiles setup ? (y/n): "
-if [ $? -ne 0 ]; then
+# Check if gum is installed
+if ! command -v gum &>/dev/null; then
+	echo "Error: gum is not installed. Install it with 'sudo pacman -S gum'"
+	exit 1
+fi
+
+if ! gum confirm "Do you want to start the full system setup?"; then
 	echo "Setup cancelled."
 	exit 1
 fi
 
 echo
 # Run the scripts in sequence
-run_script "./full_packages.sh"
-run_script "./.setup/.dotfiles.sh"
-run_script "./.setup/fonts-setup.sh"
-run_script "./.setup/zsh_setup.sh"
-run_script "./.setup/firefox.sh"
-run_script "./.setup/thunar-setup.sh"
-run_script "./.setup/pacman.sh"
-run_script "./.setup/thunar-setup.sh"
-run_script "./.setup/neovim-setup.sh"
+run_script "full_packages.sh"
+run_script "auto-setups/dotfiles.sh"
+run_script "auto-setups/zsh_setup.sh"
+run_script "auto-setups/firefox.sh"
+run_script "auto-setups/thunar-setup.sh"
+run_script "auto-setups/pacman.sh"
+run_script "auto-setups/neovim-setup.sh"
 
 echo -e "All scripts completed successfully."
 echo -e "Enjoy your system."
+
