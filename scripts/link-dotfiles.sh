13f7a6b5719164018de075e080ea02d253c0a9e5
MR-5OBOT <yassir7m7@gmail.com>
2026-04-10 18:10:57 +0100

    ..

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -16,11 +16,25 @@ link_local_bin() {
   symlink_path "${DOTFILES_DIR}/.local/bin" "${HOME}/.local/bin"
 }
 
+link_local_share_applications() {
+  local applications_dir="${DOTFILES_DIR}/.local/share/applications"
+  local item
+
+  [[ -d "${applications_dir}" ]] || return 0
+
+  mkdir -p "${HOME}/.local/share/applications"
+  for item in "${applications_dir}"/*; do
+    [[ -e "${item}" ]] || continue
+    symlink_path "${item}" "${HOME}/.local/share/applications/$(basename "${item}")"
+  done
+}
+
 main() {
   symlink_path "${DOTFILES_DIR}/.zshrc" "${HOME}/.zshrc"
   symlink_path "${DOTFILES_DIR}/.gitconfig" "${HOME}/.gitconfig"
   link_config_tree
   link_local_bin
+  link_local_share_applications
 
   if [[ -d "${ASSETS_DIR}/wallpapers" ]]; then
     mkdir -p "${HOME}/Pictures"
