008094367e6eb041b2c06a66f1c998d999d9b23a
MR-5OBOT <yassir7m7@gmail.com>
2026-06-05 11:03:00 +0100

    .

--- dotfiles/.config/hypr/hyprland.lua
+++ dotfiles/.config/hypr/hyprland.lua
@@ -8,15 +8,10 @@ require("lua_configs.animations")
 require("lua_configs.windowrules")
 require("lua_configs.keymaps")
 require("lua_configs.autostart")
+require("lua_configs.misc")
 
 -- Load any specific configurations for swaync or other modules if needed
 -- require("lua_configs.swaync")
-
-hl.config({
-	general = {
-		resize_on_border = true,
-	},
-})
 
 -- HyprMod managed settings
 require("hyprland-gui")
---
+++ dotfiles/.config/hypr/lua_configs/misc.lua
@@ -0,0 +1,5 @@
+hl.config({
+	general = {
+		resize_on_border = true,
+	},
+})
