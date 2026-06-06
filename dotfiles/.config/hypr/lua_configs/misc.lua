6ce687eee32bf73b5e7fe98fc3e193c04084f3d7
MR-5OBOT <yassir7m7@gmail.com>
2026-06-06 13:45:46 +0100

    .

--- dotfiles/.config/hypr/lua_configs/misc.lua
+++ dotfiles/.config/hypr/lua_configs/misc.lua
@@ -2,13 +2,13 @@ hl.config({
 	general = {
 		allow_tearing = true,
 		hover_icon_on_border = true,
-		snap = {
-			enabled = true,
-			respect_gaps = true,
-		},
+		-- snap = {
+		-- 	enabled = true,
+		-- 	respect_gaps = true,
+		-- },
 	},
 	input = {
 		natural_scroll = false,
-		sensitivity = -0.15,
+		sensitivity = -0.10,
 	},
 })
