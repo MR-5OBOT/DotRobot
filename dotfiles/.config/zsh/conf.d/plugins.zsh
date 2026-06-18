feed1bb21b7f15decb8cfb9520dc52041dce4f40
MR-5OBOT <yassir7m7@gmail.com>
2026-06-18 04:56:26 +0100

    .

--- dotfiles/.config/zsh/conf.d/plugins.zsh
+++ dotfiles/.config/zsh/conf.d/plugins.zsh
@@ -3,17 +3,21 @@
 # Set before zsh-autosuggestions loads.
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=60"
 
+# Keep the completion dump in the cache dir, not $ZDOTDIR (which is the repo).
+ZSH_COMPDUMP="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump-${ZSH_VERSION}"
+mkdir -p "${ZSH_COMPDUMP:h}"
+
 # ---------- zinit + plugins ----------
 if [[ -r "$ZINIT_HOME/zinit.zsh" ]]; then
   source "$ZINIT_HOME/zinit.zsh"
   zinit light zsh-users/zsh-completions
   zinit wait lucid for \
-    atload"zicompinit; zicdreplay" \
+    atload"autoload -Uz compinit; compinit -d '$ZSH_COMPDUMP'; zicdreplay" \
     aloxaf/fzf-tab \
     zsh-users/zsh-autosuggestions \
     zdharma-continuum/fast-syntax-highlighting
 else
-  autoload -Uz compinit && compinit
+  autoload -Uz compinit && compinit -d "$ZSH_COMPDUMP"
   print -P "%F{yellow}zinit not installed. Run scripts/setup-shell-tools.sh for plugin bootstrap.%f"
 fi
 
